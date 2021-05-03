50306bce6311cb3dc77369a897b99da33e6937fc
sandmandreamcatcher <dr.catcher@ukr.net>
2021-05-03 22:11:04 +0300

    Added animation to BlackHoles

---
+++ EnemySpawner/Assets/Scripts/BlackHoleRatation.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BlackHoleRatation : MonoBehaviour
+{
+    [SerializeField] private Rigidbody2D _rigidbody;
+    [SerializeField] private float _rotationSpeed;
+    [SerializeField] private float _rotationDirection = 1f; 
+
+    private void Update()
+    {
+        _rigidbody.rotation += (_rotationSpeed * _rotationDirection) * Time.deltaTime;
+    }
+}
